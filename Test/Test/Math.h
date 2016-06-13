4d9059f355e0831a1556332876ed15ef2492b66b
critty <liulilittle@users.noreply.github.com>
2016-06-13 13:32:46 +0800

    Create Math.h

---
+++ Test/Test/Math.h
@@ -0,0 +1,14 @@
+#ifndef _MATH_H
+
+#define _MATH_H
+
+#pragma comment(lib, "math.lib")
+
+extern "C" long  abs(long v);
+extern "C" long  add(long x, long y);
+extern "C" long  mod(long x, long y);
+extern "C" double  pow(double x, double y);
+extern "C" long  sub(long x, long y);
+
+#endif
+
